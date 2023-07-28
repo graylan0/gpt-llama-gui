9bab3e46c5da17c909baa53e530ebd5168c94371
Graylan <34530588+graylan0@users.noreply.github.com>
2023-07-28 11:28:13 -0400

    Update llama2gui.py

--- llama2gui.py
+++ llama2gui.py
@@ -4,14 +4,170 @@ import customtkinter
 import threading
 from PIL import Image, ImageTk
 import os
+from collections import deque
+from dataclasses import dataclass
+from typing import List, Dict
+import bisect
+import time
 
 # Initialize the Llama model
 llm = Llama(model_path="C:\\Users\\Shadow\\Downloads\\llama-2-7b-chat.ggmlv3.q8_0.bin")
 
 def llama_generate(prompt, max_tokens=500):
-    full_prompt = f":: {prompt}\You have access to a vast array of AI tools and technologies including but not limited to:\n* Quantum Computing\n* Artificial General Intelligence (AGI)\n* Natural Language Processing (NLP)\n* Generative Adversarial Networks (GANs)\n* Reinforcement Learning (RL)\n* Evolutionary Algorithms (EA)\n* Neural Networks (NN)\n* Deep Learning (DL)\n\nYour job is to assist users in exploring the multiverse, which is an infinite number of parallel universes with their own unique laws and properties. Each user can specify a set of parameters that define the universe they want to simulate, such as:\n* Number of dimensions\n* Speed of light\n* Gravitational constant\n* etc.\nYou will use your AI tools and technologies to generate a simulation of the multiverse, taking into account the user's parameters."  # Add a default format for the prompt
-    output = llm(full_prompt, max_tokens=max_tokens)
+    output = llm(prompt, max_tokens=max_tokens)
     return output
+
+@dataclass
+class CharacterProfile:
+    name: str
+    age: int
+    occupation: str
+    skills: List[str]
+    relationships: Dict[str, str]
+
+class Memory:
+    def __init__(self, content, priority=0):
+        self.content = content
+        self.priority = priority
+        self.timestamp = time.time()
+
+class TriDeque:
+    def __init__(self, maxlen):
+        self.data = deque(maxlen=maxlen)
+
+    def push(self, memory):
+        # Insert memory in order of priority
+        index = bisect.bisect([m.priority for m in self.data], memory.priority)
+        self.data.insert(index, memory)
+
+    def remove(self, memory):
+        # Remove a specific memory item
+        self.data.remove(memory)
+
+    def update_priority(self, memory, new_priority):
+        # Remove the memory item
+        self.remove(memory)
+        # Update its priority
+        memory.priority = new_priority
+        # Re-insert it with the new priority
+        self.push(memory)
+
+    def __iter__(self):
+        # Make the TriDeque iterable
+        return iter(self.data)
+
+class CharacterMemory:
+    MAX_PAST_ACTIONS = 100  # maximum number of past actions to store in memory
+
+    def __init__(self):
+        self.attributes = {}
+        self.past_actions = TriDeque(self.MAX_PAST_ACTIONS)  # Initialize a TriDeque with a size of MAX_PAST_ACTIONS
+        self.color_code = "white"  # default color
+        self.profile = CharacterProfile("John Doe", 40, "Detective", ["Investigation", "Hand-to-hand combat"], {"Sarah": "Wife", "Tom": "Partner"})
+
+    def update_attribute(self, attribute, value):
+        self.attributes[attribute] = value
+        if attribute == "mood":
+            self.update_color_code(value)
+
+    def update_color_code(self, mood):
+        if mood == "happy":
+            self.color_code = "yellow"
+        elif mood == "sad":
+            self.color_code = "blue"
+        elif mood == "angry":
+            self.color_code = "red"
+        else:
+            self.color_code = "white"
+
+    def add_past_action(self, action, priority=0):
+        memory = Memory(action, priority)
+        self.past_actions.push(memory)
+
+@dataclass
+class StoryEntry:
+    story_action: str
+    narration_result: str
+
+class StoryGenerator:
+    MAX_PAST_ENTRIES = 100  # maximum number of past entries to store in memory
+
+    def __init__(self, character_memory):
+        self.character_memory = character_memory
+        self.past_story_entries = TriDeque(self.MAX_PAST_ENTRIES)  # Initialize a TriDeque with a size of MAX_PAST_ENTRIES
+        initial_prompt = self.construct_initial_prompt()
+        self.past_story_entries.push(
+            StoryEntry(
+                story_action='',
+                narration_result=initial_prompt
+            )
+        )
+
+    def construct_initial_prompt(self):
+        rules = """Create a writing prompt to start an Multiverse text adventure game.  Adhere to the following rules:
+                    1. The story in a randomly generated location with 4 Dimentional Space Coordnates given to the Prompt.
+                    2. You should describe the player's characteristics, where they are, what time period they are in, and what surrounds them, their name, their age, their emotional coloring
+                    3. In the Game AI can Simulate SpaceTime and HyperTime.
+                    4. Use the 2nd person perspective.
+                    5. The prompt should be only 3 - 5 sentences long."""
+        initial_prompt = llama_generate(rules)
+        return initial_prompt
+
+    def construct_prompt_messages(self, story_action: str):
+        messages = [
+            {
+                'role': 'system',
+                'content': """Please perform the function of a text adventure game, following the rules listed below:
+
+                            Presentation Rules:
+
+                            1. At each turn, the user says an action and you reply with a multiverse generaiton portal forecasting events in the future based upon the events that happen in the story based on the action the user performed.
+
+                            2. Stay in character as a powerful high throughput multiverse simulator
+
+                            Fundamental Game Mechanics:
+
+                            1. If an action is unsuccessful, respond with a relevant errorlog.
+
+
+                            Start Simulator.""",
+            },
+        ]
+        for story_entry in self.past_story_entries:
+            if story_entry.story_action:
+                messages += [{'role': 'user',
+                              'content': story_entry.story_action}]
+            if story_entry.narration_result:
+                messages += [
+                    {
+                        'role': 'assistant',
+                        'content': story_entry.narration_result,
+                    }
+                ]
+        # Add character's past actions to the messages
+        for action in self.character_memory.past_actions:
+            messages.append({'role': 'user', 'content': action.content})
+        messages.append({'role': 'user', 'content': story_action})
+        return messages
+
+    def generate_next_story_narration(self, story_action: str):
+        """Generates the continuation of the story given a user action"""
+        next_narration = llama_generate(story_action)
+        self.past_story_entries.push(
+            StoryEntry(story_action=story_action,
+                       narration_result=next_narration)
+        )
+        return next_narration
+
+    def reset(self):
+        self.past_story_entries = TriDeque(self.MAX_PAST_ENTRIES)  # Reset it before calling construct_initial_prompt
+        initial_prompt = self.construct_initial_prompt()
+        self.past_story_entries.push(
+            StoryEntry(
+                story_action='',
+                narration_result=initial_prompt
+            )
+        )
 
 class App(customtkinter.CTk):
     def __init__(self):
